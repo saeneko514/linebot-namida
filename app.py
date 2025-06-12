32209f319248d60b9d3bf960ee3d1ab667e5612b
Saeko <saeko514@outlook.jp>
2025-06-13 06:54:45 +0900

    Update app.py

--- app.py
+++ app.py
@@ -111,4 +111,5 @@ if __name__ != "__main__":
     application = app
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, port=port)
